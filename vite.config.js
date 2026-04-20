b8fd1113b61ae88f53d6dae34f3bcca8bd2e1856
skylerHRG <fshh.cn@gmail.com>
2026-04-20 12:01:29 +0700

    style: 替换全站为自定义专属 Logo

--- vite.config.js
+++ vite.config.js
@@ -6,29 +6,23 @@ export default defineConfig({
   plugins: [
     react(),
     VitePWA({
-      registerType: 'autoUpdate', // 自动更新 Service Worker
-      // includeAssets: ['favicon.ico', 'apple-touch-icon.png'], // 静态资源
+      registerType: 'autoUpdate',
       manifest: {
         name: 'Brain Vault',
         short_name: 'BrainVault',
-        description: '个人数字资产中枢与 AI 第二大脑',
-        theme_color: '#4F46E5', // 匹配你 UI 的紫色主题
+        description: '个人数字资产管理中枢',
+        theme_color: '#4F46E5',
         background_color: '#f8fafc',
-        display: 'standalone', // 这个属性最重要！它会隐藏浏览器的地址栏
+        display: 'standalone',
         icons: [
           {
-           src: '/logo.png', // 大图标也暂时指向它
-           sizes: '180x180',
-           type: 'image/png'
-        }
-          {
-            src: '/pwa-192x192.png',
+            src: '/logo.png',
             sizes: '192x192',
             type: 'image/png',
             purpose: 'any maskable'
           },
           {
-            src: '/pwa-512x512.png',
+            src: '/logo.png',
             sizes: '512x512',
             type: 'image/png'
           }
